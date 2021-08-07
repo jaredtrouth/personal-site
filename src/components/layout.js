2e815db6494a95b1e25aa524c06f0e14a8628631
Jared Trouth <jared.trouth@gmail.com>
2021-08-07 12:05:17 -0500

    Add styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,17 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import { GlobalStyle } from "./globalStyle"
 
 import Header from "./header"
-import "./layout.css"
+
+const MainWrapper = styled.div`
+  margin: 0;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,25 +33,20 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <GlobalStyle />
+      <MainWrapper>
+        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+          <main>{children}</main>
+          <footer
+            style={{
+              marginTop: `2rem`,
+            }}
+          >
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+          </footer>
+      </MainWrapper>
     </>
   )
 }
